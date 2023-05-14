8231105ce524e3145b6f3b2084f33eacd9088829
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-05-14 21:36:45 +0200

    Final adjustments

--- src/Inbox/index.jsx
+++ src/Inbox/index.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import './style.css'
 
 
+
+
+
 const Inbox = ({ account, messages }) => {
+  const Unread = () => {
+    return <div className="unread">Nepřečtených zpráv: {messages}</div>
+
+  }
+
+  const NoUnread = () => {
+    return <div className="no-unread">Žádné nepřečtené zprávy</div>
+  }
+
   return (
     <div className="inbox">
       <div className="account">{account}</div>
-      {messages > 0 ? (
-        <div className="unread">Nepřečtených zpráv: {messages}</div>
-      ) : (
-        <div className="no-unread">Žádné nepřečtené zprávy</div>
-      )}
+      {messages > 0 ? <Unread /> : <NoUnread />}
     </div>
   )
 
